4af750ab01ba971e03cf150e584df7a8adbf22d8
Arsen Stepanyan <ar.step@icloud.com>
2026-03-30 22:34:01 +0200

    add solution 3.5

---
+++ chapter3/r_3_5.py
@@ -0,0 +1,26 @@
+# Explain why the plot of the function n^c is a straight line with slope c on a
+# log-log scale.
+
+# because log (n^c) = c * log(n) which is a straight line with the slope c!
+
+# On a log–log plot:
+#
+# * The x-axis shows ( \log n )
+# * The y-axis shows ( \log f(n) )
+#
+# For the function ( f(n) = n^c ):
+#
+# \log(n^c) = c\log(n)
+#
+# This has the form of a linear equation
+#
+# [
+# y = c x
+# ]
+#
+# with:
+#
+# * ( x = \log n )
+# * ( y = \log f(n) )
+#
+# So the graph is a straight line through the origin with slope ( c ).
