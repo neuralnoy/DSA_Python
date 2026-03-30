28b1e96b7a9249178a88219f3eac8b58f9a6cbf4
Arsen Stepanyan <ar.step@icloud.com>
2026-03-30 22:25:30 +0200

    add solution 3.3

---
+++ chapter3/r_3_3.py
@@ -0,0 +1,7 @@
+# The number of operations executed by algorithms A and B is 40n^2 and
+# 2n^3, respectively. Determine n0 such that A is better than B for n ≥ n0.
+
+# We need to find a solution where:
+# 40n^2 <= 2n^3 => 0 <= 2n^3 - 40n^2 => 2n^2(n-20) >= 0 => n>=20
+
+# Algorithm A becomes better than B for all n ≥ 20
