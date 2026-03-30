7add831c61aa17bb56d484ad5374e39d06eaabb1
Arsen Stepanyan <ar.step@icloud.com>
2026-03-30 22:42:43 +0200

    add solution 3.6

---
+++ chapter3/r_3_6.py
@@ -0,0 +1,5 @@
+# What is the sum of all the even numbers from 0 to 2n, for any positive
+# integer n?
+
+# The sum of all even numbers from 2 to 2n is 0 + 2 + 4 + ... + 2n =
+# 2 * (0 + 1 + 2 + 3 ... + n) = 2 * ((n * (n+1)) / 2) = n * (n+1) = n^2 + n
